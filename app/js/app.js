c259a363700381b220a299c8836fe3cfa222a9ff
Quynh Nguyen <quynh.nguyen@workday.com>
2013-09-24 21:35:08 -0700

    add parsesdk and render fake data into table

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,38 @@
-var app = angular.module("LearnAngularApp", ['ngRoute', 'ngResource']).
+//Main App
+var app = angular.module("LearnAngularApp", ['ngRoute', 'ngResource', 'ParseModule']).
 	config(function($routeProvider) {
 		$routeProvider
 			.when('/', {templateUrl: 'partials/list.html', controller: ListCtrl})
 			.otherwise({redirectTo: '/'});
+	}).
+	run(function(ParseSDK){});
+
+
+//Factory
+
+//Module
+angular.module('ParseModule', []).
+	factory('ParseSDK', function () {
+		Parse.initialize("Nr3Wg2Otho1E2Kh3tGx4ZsfXTA3NE0c190fovGyx", "sgbPpmB2OnU45fNA35FTZzSh4EOuMJHeApFCHC4q");
 	});
 
+//Controller
 var ListCtrl = function($scope, $location) {
-	$scope.test = "testing";
+	var Todo = Parse.Object.extend("todo");
+	var query = new Parse.Query(Todo);
+	query.find({
+		success: function(result) {
+			console.log(result);
+			$scope.$apply(function() {
+				$scope.items = result;
+			});
+			console.log($scope.items);
+		},
+		error: function(error) {
+			alert("ListCtrl error: " + error.message);
+		}
+	});
 }
+
+
 
